0a75ec713a3a5be53388fa221a0a991375cbc00c
Daniel Vicente <vivicente86@gmail.com>
2022-06-08 20:29:37 -0300

    Adicionei uma página chamada sobre

--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -9,7 +9,7 @@ export default function NavBar(){
       <ul>
         <li><Link className='estiloLink' to={"/"}><img src={Logo} /></Link></li>
         <li><Link className='estiloLink' to={"/"}>Home</Link></li>
-        <li className='estiloLink'>Sobre</li>
+        <li className='estiloLink'><Link to={"/sobre"}>Sobre</Link></li>
       </ul>
     </s.Nav>
   )
---
+++ src/paginas/pagErro/erroDeEndereco.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function EnderecoErro(){
+
+
+  return(
+    <h1>ERRO!!! PÁGINA NÃO ENCONTRADA</h1>
+  )
+}

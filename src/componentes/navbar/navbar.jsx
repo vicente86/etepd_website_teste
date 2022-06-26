cbb965d5c0cc6c39aca31ab86bdea45762c36dd1
Daniel Vicente <vivicente86@gmail.com>
2022-06-25 23:32:33 -0300

    home em andamento

--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -1,16 +1,41 @@
 import * as s from './estiloNavBar'
 import { Link } from 'react-router-dom'
-import Logo from '../../imagens/logoetepd.png'
+import Logo from '../../imagens/logoetepd01.png'
+import Seta from '../../imagens/setaa2.png'
 
 export default function NavBar(){
 
+
+  function listaMais(){
+    let imgseta = document.querySelector('#imgseta');
+    let ulmais = document.querySelector('#ulmais');
+
+    ulmais.classList.toggle('mostraMais');
+    imgseta.classList.toggle('giraSeta');
+  }
+
   return(
     <s.Nav>
-      <ul>
-        <li><Link className='estiloLink' to={"/"}><img src={Logo} /></Link></li>
-        <li><Link className='estiloLink' to={"/"}>Home</Link></li>
-        <li className='estiloLink'><Link to={"/sobre"}>Sobre</Link></li>
-      </ul>
+      <div>
+        <Link to={"/"}><img src={Logo} /></Link>
+      </div>
+
+      <div>
+        <ul>
+          <li><Link className='estiloLink' to={"/"}>Home</Link></li>
+          <li><Link className='estiloLink' to={"/sobre"}>Sobre</Link></li>
+          <li className='estiloLink' >Eventos</li>
+          <li className='estiloLink'>Projetos</li>
+          <li className='estiloLink dropdown1' onClick={() => listaMais()}> Mais <img src={Seta} id='imgseta'/>
+            <s.UlMais id='ulmais'>
+              <li className='estiloLink2'>Local</li>
+              <li className='estiloLink2'>Contatos</li>
+              <li className='estiloLink2'>Ajuda</li>
+              <li className='estiloLink2'>Termos e Privacidade</li>
+            </s.UlMais>
+          </li>
+        </ul>
+      </div>
     </s.Nav>
   )
 }
